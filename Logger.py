0d5770cdebdbe8adceb66d6b23b9cd9e942d4e3c
bumin <aysegul.bumin@ufl.edu>
2022-03-10 19:51:22 -0500

    Create Logger.py
    Runtime: 363 ms, faster than 5.12% of Python3 online submissions for Logger Rate Limiter.
    Memory Usage: 20.8 MB, less than 26.37% of Python3 online submissions for Logger Rate Limiter.

---
+++ Logger.py
@@ -0,0 +1,21 @@
+class Logger:
+
+    def __init__(self):
+        self.Logger = {}
+        self.Logger["logger"] = 0
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool: 
+        if message in self.Logger.keys():
+            if timestamp < self.Logger[message]+10:
+                #The current time is not suitable 
+                return False
+            else:
+                self.Logger[message] = timestamp
+                return True
+        else:
+            self.Logger[message]=timestamp
+            return True
+        
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
